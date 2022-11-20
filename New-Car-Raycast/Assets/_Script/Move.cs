dc0c26b7df4f99c63b88170ef84011052cc41865
Appi72020 <abdoulraouf72020@gmail.com>
2022-11-21 00:43:02 +0100

    using PlayerMobility project script to move the character

---
+++ New-Car-Raycast/Assets/_Script/Move.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    //[SerializeField] private float _jumpforce;
+    [SerializeField] private Rigidbody _rb;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        Vector3 VelocityDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * _speed;
+
+        _rb.velocity = -VelocityDirection;
+
+        //VelocityDirection.y=_rb.velocity.y;
+
+        //if (Input.GetKeyDown(KeyCode.Joystick1Button0))_rb.AddForce(Vector3.up * _jumpforce);
+    }
+}
